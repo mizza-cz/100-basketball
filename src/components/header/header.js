355f76d69fdf061a3ac0c1ac91a57f28dc7be349
mizza-cz <mizza.cz666@gmail.com>
2023-08-16 08:32:52 +0200

    add header-scrolled

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,31 @@
- 
+// const header = document.querySelector(".header");
+// let isScrolled = false;
+
+// function toggleHeaderClass() {
+//   if (window.pageYOffset > 0 && !isScrolled) {
+//     header.classList.add("scrolled");
+//     isScrolled = true;
+//   } else if (window.pageYOffset === 0 && isScrolled) {
+//     header.classList.remove("scrolled");
+//     isScrolled = false;
+//   }
+// }
+
+// document.addEventListener("wheel", toggleHeaderClass);
+
+const header = document.querySelector(".header-scroll");
+const sectionBelowHeader = document.querySelector(".add-scrolled"); // Замените ".section-below-header" на селектор вашей секции
+
+let isScrolled = false;
+
+function toggleHeaderClass() {
+  if (window.pageYOffset > sectionBelowHeader.offsetTop && !isScrolled) {
+    header.classList.add("scrolled");
+    isScrolled = true;
+  } else if (window.pageYOffset <= sectionBelowHeader.offsetTop && isScrolled) {
+    header.classList.remove("scrolled");
+    isScrolled = false;
+  }
+}
+
+document.addEventListener("scroll", toggleHeaderClass);
